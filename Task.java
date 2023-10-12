926bef375fe2a485ee796f7b6e308785920dd691
Gavin Hewitt <ghewitt@email.sc.edu>
2023-10-12 09:39:15 -0400

    updated task class

--- Task.java
+++ Task.java
@@ -5,8 +5,10 @@
  */
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Task {
+  private UUID id;
   private String name;
   private String description;
   private User author;
@@ -16,28 +18,29 @@ public class Task {
   private int priority;
   private float timeRequired;
   private ArrayList<Comment> comments;
+  private History history;
 
   public Task(String aName) {
 
   }
 
   public boolean changeCategory() {
-
+    return false;
   }
 
   public boolean resolve() {
-
+    return false;
   }
 
   public boolean changePriority(int priority) {
-
+    return false;
   }
 
   public boolean addComment(Comment comment) {
-
+    return false;
   }
 
   public boolean deleteComment(Comment comment) {
-    
+    return false;
   }
 }
