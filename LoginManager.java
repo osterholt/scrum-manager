230435c0eb73d4b934a669dfcbb6876de8c6d360
evieellis <evie.ellis11@gmailc.om>
2023-10-17 09:43:40 -0400

    almost complete LoginManager

--- LoginManager.java
+++ LoginManager.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 /**
- * @author Cam Osterholt
- * @version v1.0
- * Date: 10/10/2023
+ * @author Cam Osterholt & Evelyn Ellis
+ * @version v2.0
+ * Date: 10/17/2023
  */
 
 public class LoginManager {
@@ -21,12 +23,25 @@ public class LoginManager {
     }
 
     public static boolean checkEmail(String email) {
-        return true;
+        // check if email is already in user list
+        for (User user : userList) {
+            if(user.getEmail().equals(email)){
+                return false;
+            }    
+        }
+        // check if email is valid
+        String emailRegex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
+
     public static boolean checkPassword(String password) {
-        return true;
+        // check for size
+        return password.length()<8;
     }
     public boolean addUser(User newUser) {
+        userList.add(newUser);
         return true;
     }
     public static User getUser(UUID id) {
