fbe6d62cae7eaa05573a7b9b6fc768e4913dc70d
Cam Osterholt <github@osterholt.us>
2023-10-18 11:11:09 -0400

    Fixed #17

--- UI.java
+++ UI.java
@@ -5,10 +5,17 @@
  */
 
 public class UI {
+    private static final String[] USER_CAM = {"Cam", "Osterholt", "banana@osterholt.us", "p33y0urp@nt$"};
     public static void main(String args[]) {
-        run();
+        UI ui = new UI();
+        ui.run();
     }
-    public static void run() {
-        //Weee!
+    public void run() {
+        AppFacade ap = AppFacade.getInstance();
+        //TODO:
+        //ADD USER with data above
+        //Signup
+        //Add to json file - test
+        //Make sure we can save
     }
 }
