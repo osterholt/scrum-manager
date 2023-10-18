486f64a2503999fbb134d64fe34ea96feb917505
Cam Osterholt <github@osterholt.us>
2023-10-18 10:06:54 -0400

    Worked on.

--- Test.java
+++ Test.java
@@ -1,8 +1,20 @@
+/**
+ * Test class for ScrumManager
+ * @version v1.0
+ * @author Cam Osterholt & Gavin Hewitt
+ */
+
 public class Test {
     private static boolean debug = true;
 
+    //Leaderboard Testing
+
+    //TEST_USER = {_FirstName, _LastName, _Score}
+    private static final String[][] TEST_USERS = {{"Cam", "Osterholt", "cam@gmail.com", "password", "16"}, {"Evie", "Ellis", "evie@gmail.com", "password", "12"}, {"Joe", "Mama", "joe@gmail.com", "password", "26"}, {"Bill", "Clinton", "bill@gmail.com", "password", "2"}};
+
     public static void main(String[] args) {
-        //TODO
+        
+        testLeaderboard();
     }
 
     public static void print(String str) {
@@ -17,5 +29,14 @@ public class Test {
     public static boolean toggleDebug() {
         debug = !debug;
         return debug;
+    }
+
+    public static void testLeaderboard() {
+        Leaderboard lb = new Leaderboard();
+        for(String[] userStr : TEST_USERS) {
+            lb.updateScore(new User(userStr[0], userStr[1], userStr[2], userStr[3]), Integer.parseInt(userStr[4]));
+        }
+        lb.print();
+        //TODO: In progress by Cam
     }
 }
